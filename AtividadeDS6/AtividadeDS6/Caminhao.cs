d722eadf790715d809563e1bb674be521a476c40
ThiagoAquila <80552738+ThiagoAquila@users.noreply.github.com>
2022-03-25 22:03:05 -0300

    Cadastro Carro e Caminhão  prontos
    Nessa versão eu terminei os métodos de registro de carros e caminhões

---
+++ AtividadeDS6/AtividadeDS6/Caminhao.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AtividadeDS6
+{
+    class Caminhao : Veiculo
+    {
+        //Atributos
+        private int numeroEixos;
+        private double pesoMaxCarga;
+        private double distEntreEixos;
+
+        //Construtores SEM parametros
+        public Caminhao() : base ()
+        {
+            this.numeroEixos = 0;
+            this.pesoMaxCarga = 0.0;
+            this.distEntreEixos = 0.0;
+        }
+
+        //Construtores COM paramentros
+        public Caminhao(string modelo, string fabricante, int ano, string cor, int numeroPortas, string placa, int numeroEixos, double pesoMaxCarga, double distEntreEixos) 
+            : base (modelo, fabricante, ano, cor, numeroPortas, placa)
+        {
+            this.numeroEixos = numeroEixos;
+            this.pesoMaxCarga = pesoMaxCarga;
+            this.distEntreEixos = distEntreEixos;
+        }
+
+        //GET SET
+        public int getsetNumeroEixos
+        {
+            get { return numeroEixos; }
+            set { numeroEixos = value; }
+        }
+
+        public double getsetPesoMaxCarga
+        {
+            get { return pesoMaxCarga; }
+            set { pesoMaxCarga = value; }
+        }
+
+        public double getsetDistEntreEixos
+        {
+            get { return distEntreEixos; }
+            set { distEntreEixos = value; }
+        }
+    }
+}
---
+++ AtividadeDS6/AtividadeDS6/Carro.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AtividadeDS6
+{
+    class Carro : Veiculo
+    {
+        //Atributos
+        private int capPortaMala;
+        private bool bagageiro;
+        private bool estepe;
+
+        //Construtores SEM parametro
+        public Carro() : base()
+        {
+            this.capPortaMala = 0;
+            this.bagageiro = false;
+            this.estepe = false;
+        }
+
+        //Construtores COM parametro
+        public Carro(string modelo, string fabricante, int ano, string cor, int numeroPortas, string placa, int capPortaMala, bool bagageiro, bool estepe) 
+            : base(modelo, fabricante, ano, cor, numeroPortas, placa)
+        {
+            this.estepe = estepe;
+            this.capPortaMala = capPortaMala;
+            this.bagageiro = bagageiro;
+        }
+
+        //GET SET
+        public int getsetCapPortaMala
+        {
+            get { return capPortaMala; }
+            set { capPortaMala = value; }
+        }
+
+        public bool getsetBagageiro
+        {
+            get { return bagageiro; }
+            set { bagageiro = value; }
+        }
+
+        public bool getsetEstepe
+        {
+            get { return estepe; }
+            set { estepe = value; }
+        }
+    }
+}
